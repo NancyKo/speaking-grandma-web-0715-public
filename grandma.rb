51a97f5d0168a30a00a2bc2d5fccea25d2c39671
NancyKo <konancys@gmail.com>
2015-07-29 20:07:27 -0400

    FINISHED

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    # CAN"T HEAR YOU 
+    "HUH?! SPEAK UP, SONNY!"
+  else 
+    # HEARS YOU CLEARLY
+    "NO, NOT SINCE 1938!"
+  end
+end
